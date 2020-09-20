2949c200603da2062112611560ead650382f3205
Jair Milanes <weblayoutz@gmail.com>
2020-09-19 22:50:48 -0400

    chore: configuration tweeks

--- config/database.js
+++ config/database.js
@@ -1,15 +1,27 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
+module.exports = ({ env }) => {
+  let settings = {
+    client: 'sqlite',
+    filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+  }
+
+  if (env('STRAPI_DATABASE_CLIENT', 'bookshelf') === 'mongoose') {
+    settings = {
+      uri: env('STRAPI_DATABASE_URI'),
+    }
+  }
+
+
+  return {
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: env('STRAPI_DATABASE_CLIENT', 'bookshelf'),
+        settings,
+        options: {
+          ssl: true,
+          useNullAsDefault: true,
+        },
       },
     },
-  },
-});
+  }
+};
--- config/server.js
+++ config/server.js
@@ -1,9 +1,11 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env('STRAPI_HOST', '0.0.0.0'),
+  port: env.int('STRAPI_PORT', 8000),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '9174a55288b23c8382c92114f307bc2e'),
+      secret: env('STRAPI_ADMIN_JWT_SECRET'),
+      host: env('STRAPI_ADMIN_HOST', 'admin'),
+      port: env.int('STRAPI_ADMIN_PORT', 1337),
     },
   },
 });
